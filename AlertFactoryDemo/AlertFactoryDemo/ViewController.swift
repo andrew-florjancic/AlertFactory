53baa103ffa263a92ff1402add7a456bb0a96373
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-08-06 12:47:37 -0400

    Initial commit Generated boilerplate swift package and demo app

---
+++ AlertFactoryDemo/AlertFactoryDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertFactoryDemo
+//
+//  Created by Andrew Florjancic on 8/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
