c56f38f5d114baf50c2e1d5fb005398437584b88
Andrew Florjancic <andrewflorjancic@gmail.com>
2024-08-08 16:59:34 -0400

    Added DemoTableViewCell
    Created a custom UITableViewCell class to display alerts in a tableview

---
+++ AlertFactoryDemo/AlertFactoryDemo/DemoTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  DemoTableViewCell.swift
+//  AlertFactoryDemo
+//
+//  Created by Andrew Florjancic on 8/6/24.
+//  Copyright © 2024 Andrew Florjancic. All rights reserved.
+//
+
+import UIKit
+
+/// Cell for displaying different Alerts
+class DemoTableViewCell: UITableViewCell {
+    static let identifier = "DemoTableViewCell"
+    
+    /// Alert to be displayed
+    var alert: UIAlertController?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    /// Configures the cell with a provided view model
+    /// - Parameter viewModel: A model used to configure the cell data
+    func configure(with viewModel: DemoTableViewCellModel) {
+        textLabel?.text = viewModel.title
+        alert = viewModel.alert
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+/// A view model used to configre a tableView cell with a title and alert
+struct DemoTableViewCellModel {
+    let title: String
+    let alert: UIAlertController
+}
